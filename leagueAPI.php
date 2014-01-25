7044017b32a2f4760f539d18a3e4b36d135dbc42
Richard Song <brsong22@gmail.com>
2014-01-25 13:55:17 -0600

    added file responsible for accessing api

---
+++ leagueAPI.php
@@ -0,0 +1,20 @@
+<?
+	class Summoner{
+		public summoner_name = "";
+		public summoner_id = -1;
+		public summoner_level = -1;
+		public summoner_rank = "unranked";
+		
+		public function getID(summoner_name){
+			
+		}
+		
+		public function getGeneralInfo(summoner_id){
+		
+		}
+		
+		public function getRankedStats(summoner_id){
+		
+		}
+	}
+?>
